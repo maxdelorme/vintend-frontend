c54a9474977b0ffbd658442a92da41e894c3439d
Maxime Delorme <maxime@familledelorme.com>
2025-11-19 18:11:05 +0100

    Add carousel

--- src/pages/Offer.jsx
+++ src/pages/Offer.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 const Offer = () => {
   const { id } = useParams();
   const [offer, setOffer] = useState(null);
@@ -23,26 +25,37 @@ const Offer = () => {
     }
   };
 
-  // <p>_id</p>
-  // <p>product_name</p>
-  // <p>product_description</p>
-  // <p>product_price</p>
-  // <p>product_details</p>
-  // <p>product_pictures</p>
-  // <p>owner</p>
-  // <p>product_date</p>
-  // <p>product_image</p>
-  // <p>__v</p>
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 1,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
 
   return isLoading ? (
     <div>Chargement en cours....</div>
   ) : (
     <div className="offer-page">
       <div className="col-g">
-        <img
-          src={offer.product_image.secure_url}
-          alt={offer.product_description}
-        />
+        <Carousel responsive={responsive}>
+          {offer.product_pictures &&
+            offer.product_pictures.map((item, index) => (
+              <img key={item} src={item.secure_url} />
+            ))}
+        </Carousel>
       </div>
       <div className="col-d">
         <div className="price">{offer.product_price} €</div>
