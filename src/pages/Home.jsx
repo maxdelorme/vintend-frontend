b81911b86dc3914276feba988a7067ff6af8c24f
Maxime Delorme <maxime@familledelorme.com>
2025-11-19 17:41:31 +0100

    add pagination on home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,13 @@ import Offer from "../components/Offer";
 const Home = ({ search }) => {
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  var pages = 0;
+  const offerPerPage = 15;
 
   useEffect(() => {
     getData();
-  }, [search]);
+  }, [search, page]);
 
   const getData = async () => {
     try {
@@ -16,6 +19,8 @@ const Home = ({ search }) => {
         {
           params: {
             title: search,
+            page: page,
+            limit: offerPerPage,
           },
         }
       );
@@ -27,14 +32,36 @@ const Home = ({ search }) => {
     }
   };
 
+  if (!isLoading) {
+    pages = Math.ceil(data.count / offerPerPage);
+  }
+
   return isLoading ? (
     <div>Chargement en cours....</div>
   ) : (
-    <div className="listOffers">
-      {data.offers.map((item) => (
-        <Offer key={item._id} {...item}></Offer>
-      ))}
-    </div>
+    <>
+      <nav>
+        Pages :{" "}
+        <ol className="pages">
+          {new Array(pages).fill("").map((item, index) => (
+            <li
+              key={index}
+              onClick={() => {
+                setPage(index + 1);
+              }}
+              className={page === index + 1 ? "active" : ""}
+            >
+              {index + 1}
+            </li>
+          ))}
+        </ol>
+      </nav>
+      <div className="listOffers">
+        {data.offers.map((item) => (
+          <Offer key={item._id} {...item}></Offer>
+        ))}
+      </div>
+    </>
   );
 };
 
