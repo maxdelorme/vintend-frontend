b1f79c49e206380508bac221f88e2cf4f11f2f4b
Maxime Delorme <maxime@familledelorme.com>
2025-11-25 14:37:51 +0100

    Display result parameter only when on HomePage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import logo from "../../assets/img/logo.svg";
 import { MdOutlineSearch } from "react-icons/md";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import InputRange from "react-input-range";
 import "react-input-range/lib/css/index.css";
 import "./header.css";
@@ -26,56 +26,67 @@ const Header = ({
     setIsAuthenticated(false);
   };
 
+  const location = useLocation();
+
   return (
     <header>
       <div className="container">
         <Link to="/">
           <img src={logo} />
         </Link>
-        <label className="switchLabel">
-          trier par{" "}
-          <Switch isON={sort} setIsON={setSort} size="10px" color="#FF0000" />
-        </label>
-        <label>
-          <input
-            value={search}
-            onChange={(event) => {
-              const searchValue = event.target.value;
-              setSearch(searchValue);
-              if (searchValue)
-                setSearchParams((prev) => {
-                  prev.set("search", searchValue);
-                  return prev;
-                });
-              else {
-                setSearchParams((prev) => {
-                  prev.delete("search");
-                  return prev;
-                });
-              }
-            }}
-            type="text"
-            placeholder="Recherche des articles"
-          ></input>
-          <MdOutlineSearch />
-        </label>
-        <label className="range">
-          Prix&nbsp;entre&nbsp;
-          <InputRange
-            value={range}
-            maxValue={100}
-            minValue={0}
-            formatLabel={(value) => `${value} €`}
-            onChange={(range) => {
-              setRange(range);
-              setSearchParams((prev) => {
-                prev.set("priceMin", range.min);
-                prev.set("priceMax", range.max);
-                return prev;
-              });
-            }}
-          />
-        </label>
+        {location.pathname === "/" && (
+          <>
+            <label className="switchLabel">
+              trier par{" "}
+              <Switch
+                isON={sort}
+                setIsON={setSort}
+                size="10px"
+                color="#FF0000"
+              />
+            </label>
+            <label>
+              <input
+                value={search}
+                onChange={(event) => {
+                  const searchValue = event.target.value;
+                  setSearch(searchValue);
+                  if (searchValue)
+                    setSearchParams((prev) => {
+                      prev.set("search", searchValue);
+                      return prev;
+                    });
+                  else {
+                    setSearchParams((prev) => {
+                      prev.delete("search");
+                      return prev;
+                    });
+                  }
+                }}
+                type="text"
+                placeholder="Recherche des articles"
+              ></input>
+              <MdOutlineSearch />
+            </label>
+            <label className="range">
+              Prix&nbsp;entre&nbsp;
+              <InputRange
+                value={range}
+                maxValue={100}
+                minValue={0}
+                formatLabel={(value) => `${value} €`}
+                onChange={(range) => {
+                  setRange(range);
+                  setSearchParams((prev) => {
+                    prev.set("priceMin", range.min);
+                    prev.set("priceMax", range.max);
+                    return prev;
+                  });
+                }}
+              />
+            </label>
+          </>
+        )}
         <span>
           {isAuthenticated ? (
             <button className="alert" onClick={disconnect}>
