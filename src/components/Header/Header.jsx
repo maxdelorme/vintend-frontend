1af80983301bb3cfc86a2c60c5b17196b1e1f887
Maxime Delorme <maxime@familledelorme.com>
2025-11-26 12:31:55 +0100

    Make header responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import logo from "../../assets/img/logo.svg";
-import { IoIosSearch } from "react-icons/io";
+import { IoIosSearch, IoIosMenu } from "react-icons/io";
 import { Link, useLocation } from "react-router-dom";
 import InputRange from "react-input-range";
 import "react-input-range/lib/css/index.css";
@@ -8,6 +8,7 @@ import Cookies from "js-cookie";
 import LoginForm from "../LoginForm/LoginForm";
 import SignupForm from "../SignupForm/SignupForm";
 import Switch from "../switch/Switch";
+import useWindowDimensions from "../../utils/useWindowSize";
 
 const Header = ({
   search,
@@ -27,111 +28,119 @@ const Header = ({
   };
 
   const location = useLocation();
-
+  const { height, width } = useWindowDimensions();
+  const openMenu = width > 768;
   return (
     <header>
       <div className="container">
         <Link to="/">
           <img src={logo} />
         </Link>
-        {location.pathname === "/" && (
-          <>
-            <label className="switchLabel">
-              trier par{" "}
-              <Switch
-                isON={sort}
-                setIsON={setSort}
-                size="10px"
-                color="#FF0000"
-              />
-            </label>
-            <label>
-              <input
-                value={search}
-                onChange={(event) => {
-                  const searchValue = event.target.value;
-                  setSearch(searchValue);
-                  if (searchValue)
+        <details open={openMenu}>
+          <summary>
+            <IoIosMenu />
+          </summary>
+
+          {location.pathname === "/" && (
+            <div className="filter">
+              <label className="switchLabel">
+                trier par prix{" "}
+                <Switch
+                  isON={sort}
+                  setIsON={setSort}
+                  size="10px"
+                  color="#FF0000"
+                />
+              </label>
+              <label>
+                <input
+                  value={search}
+                  onChange={(event) => {
+                    const searchValue = event.target.value;
+                    setSearch(searchValue);
+                    if (searchValue)
+                      setSearchParams((prev) => {
+                        prev.set("search", searchValue);
+                        return prev;
+                      });
+                    else {
+                      setSearchParams((prev) => {
+                        prev.delete("search");
+                        return prev;
+                      });
+                    }
+                  }}
+                  type="text"
+                  placeholder="Recherche des articles"
+                ></input>
+                <IoIosSearch />
+              </label>
+              <label className="range">
+                Prix&nbsp;entre&nbsp;
+                <InputRange
+                  value={range}
+                  maxValue={100}
+                  minValue={0}
+                  formatLabel={(value) => `${value} €`}
+                  onChange={(range) => {
+                    setRange(range);
                     setSearchParams((prev) => {
-                      prev.set("search", searchValue);
+                      prev.set("priceMin", range.min);
+                      prev.set("priceMax", range.max);
                       return prev;
                     });
-                  else {
-                    setSearchParams((prev) => {
-                      prev.delete("search");
-                      return prev;
-                    });
-                  }
-                }}
-                type="text"
-                placeholder="Recherche des articles"
-              ></input>
-              <IoIosSearch />
-            </label>
-            <label className="range">
-              Prix&nbsp;entre&nbsp;
-              <InputRange
-                value={range}
-                maxValue={100}
-                minValue={0}
-                formatLabel={(value) => `${value} €`}
-                onChange={(range) => {
-                  setRange(range);
-                  setSearchParams((prev) => {
-                    prev.set("priceMin", range.min);
-                    prev.set("priceMax", range.max);
-                    return prev;
-                  });
-                }}
-              />
-            </label>
-          </>
-        )}
-        <span>
-          {isAuthenticated ? (
-            <button className="alert" onClick={disconnect}>
-              Se déconnecter
-            </button>
-          ) : (
-            <>
-              <button
-                className="outline"
-                onClick={() =>
-                  setModal({
-                    isVisible: true,
-                    children: (
-                      <SignupForm
-                        setIsAuthenticated={setIsAuthenticated}
-                        setModal={setModal}
-                      ></SignupForm>
-                    ),
-                  })
-                }
-              >
-                S'inscrire
-              </button>
-              <button
-                className="outline"
-                onClick={() =>
-                  setModal({
-                    isVisible: true,
-                    children: (
-                      <LoginForm
-                        setIsAuthenticated={setIsAuthenticated}
-                        setModal={setModal}
-                      ></LoginForm>
-                    ),
-                  })
-                }
-              >
-                Se connecter
-              </button>
-            </>
+                  }}
+                />
+              </label>
+            </div>
           )}
-          <button className="fill-primary">
-            <Link to="/publish">Vends tes articles</Link>
-          </button>
-        </span>
+
+          <span className="buttons">
+            {isAuthenticated ? (
+              <button className="alert" onClick={disconnect}>
+                Se déconnecter
+              </button>
+            ) : (
+              <>
+                <button
+                  className="outline"
+                  onClick={() =>
+                    setModal({
+                      isVisible: true,
+                      children: (
+                        <SignupForm
+                          setIsAuthenticated={setIsAuthenticated}
+                          setModal={setModal}
+                        ></SignupForm>
+                      ),
+                    })
+                  }
+                >
+                  S'inscrire
+                </button>
+                <button
+                  className="outline"
+                  onClick={() =>
+                    setModal({
+                      isVisible: true,
+                      children: (
+                        <LoginForm
+                          setIsAuthenticated={setIsAuthenticated}
+                          setModal={setModal}
+                        ></LoginForm>
+                      ),
+                    })
+                  }
+                >
+                  Se connecter
+                </button>
+              </>
+            )}
+            <button className="fill-primary">
+              <Link to="/publish">Vends tes articles</Link>
+            </button>
+          </span>
+        </details>
       </div>
     </header>
   );
